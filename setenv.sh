f7000bb31da355a378331eb2fdc790182ca6a1d0
Tsuibin <xubin@linuxdeepin.com>
2015-08-27 13:04:25 +0800

    update setenv

--- setenv.sh
+++ setenv.sh
@@ -1,2 +1,1 @@
-export PATH=/opt/FriendlyARM/toolschain/4.4.3/arm-none-linux-gnueabi/bin:$PATH
-export PATH=/opt/FriendlyARM/toolschain/4.4.3/bin:$PATH
+export PATH=/opt/FriendlyARM/toolschain/4.4.3/bin/:$PATH
